pinobatch/pently

commit 402d071dd3b0bfaa071aec42a97f37a2f0e49dbc

1- and 2-frame arps; fix #19

src/pentlymusic.s
@@ -562,6 +562,9 @@ patcmdhandlers:
   .addr handle_ch_volume-1
 
   .addr handle_portamento-1
+  .addr handle_portamento-1  ; Reserved for future use
+  .addr handle_fastarp-1
+  .addr handle_slowarp-1
 
 num_patcmdhandlers = (* - patcmdhandlers) / 2
 .popseg
@@ -591,8 +594,26 @@ handle_arpeggio:
     sta arpInterval2,x
   :
   jmp nextPatternByte
+handle_fastarp:
+  cpx #12
+  bcs :+
+    lda #%10111111
+    and arpPhase,x
+    sta arpPhase,x
+  :
+  jmp anotherPatternByte
+handle_slowarp:
+  cpx #12
+  bcs :+
+    lda #%01000000
+    ora arpPhase,x
+    sta arpPhase,x
+  :
+  jmp anotherPatternByte
 .else
   handle_arpeggio = nextPatternByte
+  handle_fastarp = anotherPatternByte
+  handle_slowarp = anotherPatternByte
 .endif
 
 handle_legato:
@@ -694,8 +715,9 @@ instrument_id = pently_zptemp + 1
     cpx #DRUM_TRACK
     bcs skipSustainPart
       .if ::PENTLY_USE_ATTACK_TRACK || ::PENTLY_USE_ARPEGGIO
-        lda #0
-        sta arpPhase,x  ; bits 2-0: arp phase; 7: is attack injected
+        lda #%01000000
+        and arpPhase,x
+        sta arpPhase,x  ; keep only bit 6: is arp fast
       .endif
       .if ::PENTLY_USE_VIBRATO
         lda #23
@@ -710,11 +732,12 @@ instrument_id = pently_zptemp + 1
     pha
     .if ::PENTLY_USE_ATTACK_TRACK
       cpx #ATTACK_TRACK
-      bcc notAttackChannel
+      bcc notAttackTrack
         ldx attackChannel
         lda #$80  ; Disable arpeggio, vibrato, and legato until sustain
+        ora arpPhase,x
         sta arpPhase,x
-      notAttackChannel:
+      notAttackTrack:
     .endif
     lda notenum
     sta attackPitch,x
@@ -748,7 +771,9 @@ out_pitch    = pently_zptemp + 3
 out_pitchadd = pently_zptemp + 4
 
   lda pently_music_playing
-  beq silenced
+  bne :+
+    jmp silenced
+  :
   lda graceTime,x
   beq nograce
   dec graceTime,x
@@ -816,26 +841,36 @@ storePitchWithArpeggio:
 
 .if ::PENTLY_USE_ARPEGGIO
   stx xsave
-  lda #$7F
-  and arpPhase,x
+  
+  ; 7: attack is injected
+  ; 6: slow
+  ; 2-1: phase
+  ; 0: phase low bit
+  lda #$3F
+  cmp arpPhase,x
+  lda #0
+  rol a  ; A = 0 for slow arp or 1 for fast arp
+  ora arpPhase,x
+  and #$07
   tay
+  and #$06
   beq bumpArpPhase
 
   ; So we're in a nonzero phase.  Load the interval.
-  clc
+  lsr a
   adc xsave
   tax
   lda arpInterval1-1,x
 
-  ; If phase 2's interval is 0, cycle through two phases (1, 2)
-  ; instead of three (0, 1, 2).
+  ; If phase 2's interval is 0, cycle through two phases (2, 4)
+  ; or four (2-5) instead of three (0, 1, 2) or six (0-5).
   bne bumpArpPhase
-  cpy #2
+  cpy #4
   bcc bumpArpPhase
   ldy #0
 bumpArpPhase:
   iny
-  cpy #3
+  cpy #6
   bcc noArpRestart
   ldy #0
 noArpRestart:
@@ -846,8 +881,13 @@ noArpRestart:
   sta out_pitch
   ldx xsave
   tya
+
+  ; Combine new arp phase with old arp rate setting
+  eor arpPhase,x
+  and #%00000111
+  eor arpPhase,x
 .else
-  ; If arpeggio is off, just clear the attack injection flag
+  ; If arpeggio support is off, just clear the attack injection flag
   lda #0
 .endif
   sta arpPhase,x

src/musicseq.pently
@@ -899,7 +899,7 @@ song arp_waltz
     f, a a c, d, e,
   pattern melody with stringlead on pulse2
     relative
-    EN-M
+    EN-M ENP2
     a'2:-M/2 bb4:-dim c2 d4:-m
     c bb2:-M/1 w4 bb:-M/1 c
     d2:-m c4 bb2:-M/1 a4:-m
@@ -912,12 +912,12 @@ song arp_waltz
 
   pattern interlude with argtwinkle on pulse2
     relative
-    f''2.:no3 f:power
-    bb:no3 f:M/2
-    g:no3 g:power
+    ENP2 f''2.:no3 ENP1 f:power
+    ENP2 bb:no3 ENP1 f:M/2
+    ENP2 g:no3 ENP1 g:power
     c:sus4 g:M/2
-    f2.:no3 f:power
-    bb:no3 bb:Mno5
+    ENP2 f2.:no3 ENP1 f:power
+    ENP2 bb:no3 ENP1 bb:Mno5
     g:power g:M/2
     f:Mno5 w
 

src/pentlyseq.inc
@@ -245,6 +245,9 @@ GRACE = $DD
 VIBRATO = $DE
 CHVOLUME = $DF
 BEND = $E0
+RESERVEDFX1 = $E1
+FASTARP = $E2
+SLOWARP = $E3
 PATEND = $FF
 
 ; The default duration is one row (a sixteenth note in the tracker).

tools/pentlyas.py
@@ -222,6 +222,9 @@ Return None (if arp is falsey), 2 hex digits, or '-' followed by
         if not name[:1].isalpha():
             raise ValueError("chord name %s must begin with a letter"
                              % name)
+        if name in ('P1', 'P2'):
+            raise ValueError("chord name %s is reserved for rate changes"
+                             % name)
         try:
             olddefinition = self.translate_arp_name(name)
         except KeyError:
@@ -992,7 +995,13 @@ EN(-?(?:
                     if self.pitchctx.octave_mode != 'drum'
                     else None)
         if arpmatch:
-            self.pitchctx.set_arp(arpmatch.group(1))
+            arpvalue = arpmatch.group(1)
+            if arpvalue == 'P1':
+                self.notes.append("FASTARP")
+            elif arpvalue == 'P2':
+                self.notes.append("SLOWARP")
+            else:
+                self.pitchctx.set_arp(arpvalue)
             return
         if word.startswith("EN") and not arpmatch:
             print("warning: malformed arpeggio %s" % repr(word),

commit eb7be29c8ac91133f5cc032364a80db1f2a722f9

clear injection flag when attack ends
The fix for #19 around 402d071dd3b0bfaa071aec42a97f37a2f0e49dbc
(2017-09-19) caused a regression where an attack wouldn't clear
the injection flag once it ends.  This caused portamento in
"Individual features" to sound snapped to semitones.

src/pentlymusic.s
@@ -925,9 +925,9 @@ noArpRestart:
   ldx xsave
   tya
 
-  ; Combine new arp phase with old arp rate setting
+  ; Combine new arp phase and non-inject setting with old arp rate setting
   eor arpPhase,x
-  and #%00000111
+  and #%10000111
   eor arpPhase,x
 .else
   ; If arpeggio support is off, just clear the attack injection flag
@@ -1025,7 +1025,7 @@ notCutNote:
 VIBRATO_PERIOD = 12
 
 calc_vibrato:
-  .if ::PENTLY_USE_PORTAMENTO && ::PENTLY_USE_ATTACK_PHASE
+  .if ::PENTLY_USE_PORTAMENTO && ::PENTLY_USE_ATTACK_TRACK
     ; Don't apply portamento to injected attacks
     lda arpPhase,x
     bmi is_injected

tools/noise_freqs.py
@@ -30,7 +30,7 @@ def main():
     xpositions = []
     lines = ["""{| class="wikitable"
 |+ Pitches of 93-step noise on NTSC
-! Period setting || Sample rate || Fundamental || Note number || Pitch"""]
+! Period setting || Sample rate || Fundamental || MIDI note || Pitch"""]
     for i, period in enumerate(noiseperiods):
         updatefreq = 39375000/22/period
         fundamental = updatefreq/93
@@ -41,7 +41,7 @@ def main():
             "%.1f Hz" % updatefreq,
             "%.1f Hz" % fundamental,
             "%.2f" % (cts / 100),
-            "%s + %d¢" % (lynotename(notefloor), ctsdiff)
+            "<nowiki>%s</nowiki> + %d¢" % (lynotename(notefloor), ctsdiff)
         )
         lines.append("|-\n| " + " || ".join(row))
 